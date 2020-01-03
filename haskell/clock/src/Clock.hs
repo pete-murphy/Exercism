55e9b44359a642ba44f3587059bc1d9e4bc7cc37
Peter Murphy <pete@simspace.com>
2020-01-03 10:52:01 -0500

    Iterate haskell/clock

--- haskell/clock/src/Clock.hs
+++ haskell/clock/src/Clock.hs
@@ -18,14 +18,20 @@ instance Eq Clock where
   (==) = (==) `on` show
 
 fromHourMin :: Int -> Int -> Clock
-fromHourMin h m = addDelta h m (Clock 0 0)
+fromHourMin h m = Clock (h + m1) m2
+  where
+    (m1, m2) = divMod m 60
 
 toString :: Clock -> String
-toString (Clock h m) = pad 24 h ++ ":" ++ pad 60 m
+toString (Clock h m) = padHours h ++ ":" ++ padMinutes m
   where
-    pad m' (show . flip mod m' -> n)
-      | length n > 1 = n
-      | otherwise = '0' : n
+    padHours (show . flip mod 24 -> h')
+      | h' == "00" = "24"
+      | length h' > 1 = h'
+      | otherwise = '0' : h'
+    padMinutes (show . flip mod 60 -> m')
+      | length m' > 1 = m'
+      | otherwise = '0' : m'
 
 addDelta :: Int -> Int -> Clock -> Clock
 addDelta hour min' (Clock h m) = Clock (h + hour + m1) m2
