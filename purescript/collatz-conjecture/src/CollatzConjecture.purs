6e7ef85eaffdaaca17becd2c953be097c636d938
Peter Murphy <pete@simspace.com>
2020-11-01 11:08:09 -0500

    Refactor purescript/collatz-conjecture

--- purescript/collatz-conjecture/src/CollatzConjecture.purs
+++ purescript/collatz-conjecture/src/CollatzConjecture.purs
@@ -3,20 +3,28 @@ module CollatzConjecture
   ) where
 
 import Prelude
-import Data.Maybe (Maybe(..))
+import Control.Monad.ST.Internal (ST, STRef)
 import Control.Monad.ST.Internal as ST
 import Data.Int (even)
+import Data.Maybe (Maybe(..))
 
 collatz :: Int -> Maybe Int
 collatz n
   | n == 1 = Just 0
   | n < 1 = Nothing
-  | otherwise =
-    Just
-      $ ST.run do
-          m <- ST.new n
-          acc <- ST.new 0
-          ST.while ((_ /= 1) <$> ST.read m)
-            $ ST.modify (\m' -> if even m' then div m' 2 else 3 * m' + 1) m
-            *> ST.modify (_ + 1) acc
-          ST.read acc
+  | otherwise = Just (countSteps n)
+
+countSteps :: Int -> Int
+countSteps n =
+  ST.run do
+    m <- ST.new n
+    stepCount <- ST.new 0
+    ST.while ((_ /= 1) <$> ST.read m) do
+      runStep m *> increment stepCount
+    ST.read stepCount
+
+runStep :: forall r. STRef r Int -> ST r Int
+runStep m = ST.modify (\m' -> if even m' then m' `div` 2 else 3 * m' + 1) m
+
+increment :: forall r. STRef r Int -> ST r Int
+increment = ST.modify (_ + 1)
